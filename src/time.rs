5c171caecd64a430ad4da4db1edb4fabe369b56f
Matias Fontanini <matias.fontanini@gmail.com>
2025-03-17 12:22:23 -0700

    feat: add more checks when minting

---
+++ src/time.rs
@@ -0,0 +1,16 @@
+use chrono::{DateTime, Utc};
+
+/// A trait to abstract fetching the current time.
+#[cfg_attr(test, mockall::automock)]
+pub trait TimeService: Send + Sync + 'static {
+    fn current_time(&self) -> DateTime<Utc>;
+}
+
+/// A time service that uses the system clock.
+pub struct DefaultTimeService;
+
+impl TimeService for DefaultTimeService {
+    fn current_time(&self) -> DateTime<Utc> {
+        Utc::now()
+    }
+}
