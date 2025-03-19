d854df546854550fa11cbe921121512a53f7ad82
Matias Fontanini <matias.fontanini@gmail.com>
2025-03-19 09:48:59 -0700

    chore: log when running migrations

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,5 @@
 use sqlx::{Pool, Postgres};
+use tracing::info;
 
 pub mod account;
 
@@ -8,7 +9,9 @@ pub struct PostgresPool(Pool<Postgres>);
 impl PostgresPool {
     pub async fn new(url: &str) -> anyhow::Result<Self> {
         let pool = Pool::connect(url).await?;
+        info!("Running migrations");
         sqlx::migrate!().run(&pool).await?;
+        info!("Migrations ran successfully");
         Ok(Self(pool))
     }
 }
